d6c55adcc4a443e1a9c47103d481de9c728ef57c
Sander van den Berg <svdberg@me.com>
2011-10-27 18:29:21 +0200

    fixed the tests..

--- test/wordlegen/test/filter.clj
+++ test/wordlegen/test/filter.clj
@@ -3,13 +3,17 @@
   (:use [clojure.test]))
 
 (deftest test-filter1
-  (is 0 ( filter1 ""))
-  (is 3 ( filter1 "thre"))
-  (is 19 ( filter1 "thisisaverylongword")))
+  (is (=  0 ( filter1 "")))
+  (is (= 3  ( filter1 "thr")))
+  (is (= 19 ( filter1 "thisisaverylongword"))))
 
 (deftest test-filter2
+  "Do we want the last word of a tweet?"
   (let [example-tweet "this is an example tweet text"
         example-tweet-last-words "is an example tweet text"
-        result-list '((example-tweet-last-words) (example-tweet-last-words))]
-    (is  result-list (filter2 [example-tweet, example-tweet]))
-    (is [] (filter2 []))))
+        result-list '(example-tweet-last-words example-tweet-last-words)]
+    (is (= result-list (filter2 [example-tweet, example-tweet])))
+    (is (= [] (filter2 [])))))
+
+(deftest test-filter3
+  )
