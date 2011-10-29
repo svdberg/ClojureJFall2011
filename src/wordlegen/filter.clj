046ec8232ee0ea87fb574902ad6acb9ad1c237cc
Maurits <maurits.rijk@gmail.com>
2011-10-29 17:11:54 +0200

    Implemented 5th example

--- src/wordlegen/filter.clj
+++ src/wordlegen/filter.clj
@@ -17,5 +17,10 @@
 (defn filter5
   "This function returns a map with the user as key, #followers as value"
   [raw-tweets]
-  nil ; replace by your implementation!
+
+  (let [users (map :user raw-tweets)
+        names (map :name users)
+        followers (map :followers_count users)]
+    (zipmap names followers))
+    ; {"Maurits" 271} ; replace by your implementation!
   )
